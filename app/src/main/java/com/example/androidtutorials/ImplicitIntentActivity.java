62be7dd7c56c08174804b3c56879e9f2f2a0054d
Bhakti Daraniya <180540107035@darshan.ac.in>
2021-11-29 20:34:06 +0530

    Implicit Intent and Activity Lifecycle

--- app/src/main/java/com/example/androidtutorials/ImplicitIntentActivity.java
+++ app/src/main/java/com/example/androidtutorials/ImplicitIntentActivity.java
@@ -8,9 +8,12 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class ImplicitIntentActivity extends AppCompatActivity {
-    EditText etSearch;
+    EditText etSearch,etSend;
+    TextView tvScrollView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +21,8 @@ public class ImplicitIntentActivity extends AppCompatActivity {
         setContentView(R.layout.activity_implicit_intent);
 
         etSearch = findViewById(R.id.etSearch);
+        etSend = findViewById(R.id.etSend);
+        tvScrollView = findViewById(R.id.tvScrollView);
     }
 
     public void clickIntent(View view) {
@@ -41,11 +46,23 @@ public class ImplicitIntentActivity extends AppCompatActivity {
                 break;
 
             case R.id.btnSearch:
-                Intent searchIntent = new Intent(Intent.ACTION_WEB_SEARCH);
+                Intent searchIntent = new Intent(Intent.ACTION_VIEW);
                 String query = etSearch.getText().toString();
                 searchIntent.putExtra(SearchManager.QUERY,query);
                 startActivity(searchIntent);
 
+            case R.id.btnSend:
+
+                String msg = etSend.getText().toString();
+
+                Intent sendIntent = new Intent();
+                sendIntent.setAction(Intent.ACTION_SEND);
+                sendIntent.putExtra(Intent.EXTRA_TEXT,msg+" ");
+                sendIntent.setType("text/plain");
+
+
+                Intent shareIntent = Intent.createChooser(sendIntent, null);
+                startActivity(shareIntent);
         }
     }
 
